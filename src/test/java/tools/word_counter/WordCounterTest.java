4ed30b48ac1d174882a864e5fea2947b84abbb59
Tomek W <tom.wozniczka@gmail.com>
2016-09-30 20:53:31 +0200

    Word Counter - adding new word - more tests

--- src/test/java/tools/word_counter/WordCounterTest.java
+++ src/test/java/tools/word_counter/WordCounterTest.java
@@ -7,6 +7,8 @@ import static org.assertj.core.api.Assertions.*;
 public class WordCounterTest {
 
     private static final String WORD1 = "Guitar";
+    private static final String WORD2 = "Strings";
+    private static final String WORD3 = "Amplifier";
 
     private final WordCounter wordCounter = new WordCounter();
 
@@ -15,5 +17,17 @@ public class WordCounterTest {
         wordCounter.addWord(WORD1);
 
         assertThat(wordCounter.checkIfWordExists(WORD1)).isTrue();
+    }
+
+    @Test
+    public void shouldBeAbleToAddMoreThanOneWord() {
+        wordCounter.addWord(WORD1);
+        wordCounter.addWord(WORD2);
+        wordCounter.addWord(WORD3);
+
+        assertThat(wordCounter.checkIfWordExists(WORD1)).isTrue();
+        assertThat(wordCounter.checkIfWordExists(WORD2)).isTrue();
+        assertThat(wordCounter.checkIfWordExists(WORD3)).isTrue();
+
     }
 }
